38bb98527b461ea139c7a03592e987ce5b7d6286
lucksaug <lucaug2000@outlook.com>
2020-10-25 16:14:44 -0300

    Insertion Sort

--- src/sortingAlgorithms.h
+++ src/sortingAlgorithms.h
@@ -3,8 +3,18 @@ void mergeSort(int *list){
 
 }
 
-void insertionSort(){
-
+void insertionSort(int *vet, int tam){
+    int a, b, c;
+    // no Insertion Sort sempre começa na segunda posição do vetor
+    for(a = 1; a < tam; a++){
+        c = vet[a];
+        b = a - 1;
+        while(b >= 0 && c < vet[b]){
+            vet[b + 1] = vet[b];
+            b--;
+        }
+    vet[b + 1] = c;
+    }
 }
 
 void quickSort(){
