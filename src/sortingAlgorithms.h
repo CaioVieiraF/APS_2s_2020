3974f343d02daa34f5f149fce1cc808837c163c2
Gustao-DEL-SANTOS <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2020-11-09 12:24:56 -0300

    ADD METODO QUICKSORT

--- src/sortingAlgorithms.h
+++ src/sortingAlgorithms.h
@@ -63,6 +63,49 @@ void insertionSort(int *vet, int tam){
     }
 }
 
+
+//Algoritmo de ordenacao QuickSort
+void QuickSort(int* v, int tam){
+	int j = tam, k;
+	if (tam <= 1)
+		return;
+	else {
+			int x = v[0];
+			int a = 1;
+			int b = tam-1;
+		do{
+		while((a < tam) && (v[a] <= x))
+			a++;
+		while(v[b] > x)
+			b--;
+		if(a < b){
+			// faz troca
+			int temp = v[a];
+			v[a] = v[b];
+			v[b] = temp;
+			a++;
+			b--;
+	}
+		for(k = 0; k < j; k++)
+			printf("%d ", v[k]);
+			printf("\n");
+		}while(a <= b);
+	
+		// troca pivo
+		v[0] = v[b];
+		v[b] = x;
+		
+		// ordena sub-vetores restantes	
+		QuickSort(v, b);
+		QuickSort(&v[a], tam-a);
+		for(k = 0; k < j; k++)
+		printf("%d ", v[k]);
+		printf("\n");
+
+	}
+	
+}
+
 void quickSort(){
 
 }
