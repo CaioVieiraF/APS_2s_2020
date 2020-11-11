64238686916d62589e32d8a54b2298b6edfe29f7
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-11-11 18:42:35 -0300

    testes finalizados

--- src/app.cpp
+++ src/app.cpp
@@ -2,6 +2,8 @@
 // Importação das bibliotecas padrão
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <vector>
 
 // Importação dos métodos de ordenação implementados em um header
 #include "sortingAlgorithms.h"
@@ -34,27 +36,66 @@ struct Timer {
 
 void printList(int list[], int size);
 
+void teste(std::ifstream &infoDesordFile);
+
 // Protótipos das funções de teste para cada método de ordenação
 void testeMerge(int *array);
 void testeInsertion(int *array);
 void testeQuick(int *array);
 
+const int TAM = 100000;
+
 // Método principal
 int main(){
 
-    // Declaração da lista de valores desordenados
-    int array[] = {1, 3, 6, 2, 45, 65, 9};
+    // Teste com lista 100% desordenada
+    std::ifstream infoDesordFile("db/arquivo.txt");
+    std::cout << "Teste com lista 100% desordenada\n";
+    teste(infoDesordFile);
+    infoDesordFile.close();
+    std::cout << "================================\n";
 
-    // teste de tempo do insertion sort.
-    testeInsertion(array);
+    // Teste com lista 50% desordenada
+    std::ifstream info50DesordFile("db/arquivo_50.txt");
+    std::cout << "Teste com lista 50% desordenada\n";
+    teste(info50DesordFile);
+    info50DesordFile.close();
+    std::cout << "================================\n";
 
-    // teste de tempo do merge sort.
-    testeMerge(array);
-
-    // teste de tempo do quick sort.
-    testeQuick(array);
+    // Teste com lista 70% desordenada
+    std::ifstream info30DesordFile("db/arquivo_30.txt");
+    std::cout << "Teste com lista 70% desordenada\n";
+    teste(info30DesordFile);
+    info30DesordFile.close();
+    std::cout << "================================\n";
 
     return(0);
+}
+
+void teste(std::ifstream &infoDesordFile){
+
+    // Declaração do vetor de valores desordenados
+    int infoDesord_00[TAM], infoDesord_01[TAM], infoDesord_02[TAM];
+
+    {
+        int n, i = 0;
+        while(infoDesordFile >> n){
+            infoDesord_00[i] = n;
+            infoDesord_01[i] = n;
+            infoDesord_02[i] = n;
+            i++;
+        }
+    }
+
+    // teste de tempo do quick sort.
+    testeInsertion(infoDesord_00);
+
+    // teste de tempo do merge sort.
+    testeMerge(infoDesord_01);
+
+    // teste de tempo do insertion sort.
+    testeQuick(infoDesord_02);
+
 }
 
 // Função de teste do merge sort
@@ -63,7 +104,7 @@ void testeMerge(int *array){
     std::cout << "Merge Sort:\n";
     // Declaração do timer para contar o tempo da função mergeSort
     Timer timer;
-    mergeSort(array, 0, 7);
+    mergeSort(array, 0, TAM);
 }
 
 // Função de teste do insertion sort
@@ -72,7 +113,7 @@ void testeInsertion(int *array){
     std::cout << "Insertion Sort:\n";
     // Declaração do timer para contar o tempo da função insertionSort
     Timer timer;
-    insertionSort(array, 7);
+    insertionSort(array, TAM);
 }
 
 // Função de teste do quick sort
@@ -81,13 +122,13 @@ void testeQuick(int *array){
     std::cout << "Quick Sort:\n";
     // Declaração do timer para contar o tempo da função testeSort
     Timer timer;
-    quickSort(array, 7);
+    quickSort(array, TAM);
 }
 
 // Função que lista todos os itens de um array
 // isso é usado para debug
 void printList(int list[], int size){
-    for (int i = 0; i < size; ++i)  {
+    for (int i = 0; i < size; i++) {
         std::cout << "list[" << i << "] = " << list[i] << std::endl;
     }
     std::cout << "\n\n";
