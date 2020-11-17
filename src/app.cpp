07b2eb17f68228e25014c9afa399ffcb80898510
Gustao-DEL-SANTOS <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2020-11-17 10:51:40 -0300

    correção de palavras

--- src/app.cpp
+++ src/app.cpp
@@ -1,28 +1,28 @@
 
-// Importa√ß√£o das bibliotecas padr√£o
+// ImportaÁ„o das bibliotecas padr„o
 #include <iostream>
 #include <ctime>
 #include <fstream>
 #include <vector>
 
-// Importa√ß√£o dos m√©todos de ordena√ß√£o implementados em um header
+// ImportaÁ„o dos mÈtodos de ordenaÁ„o implementados em um header
 #include "sortingAlgorithms.h"
 
 // Struct que inicia um timer
 struct Timer {
 
-    // Declara√ß√£o das variaveis usadas nesse escopo para obter os tempos inicial e
-    // final para obter a dura√ß√£o.
+    // DeclaraÁ„o das variaveis usadas nesse escopo para obter os tempos inicial e
+    // final para obter a duraÁ„o.
     clock_t start, end;
     double duration;
 
-    // M√©todo construtor que pega o tempo atual no momento da declara√ß√£o de um objeto
+    // MÈtodo construtor que pega o tempo atual no momento da declaraÁ„o de um objeto
     Timer(){
         start = clock();
     }
 
-    // M√©todo desconstrutor que pega o tempo atual no momento da "destrui√ß√£o" do objeto,
-    // calcula a dura√ß√£o total da vida do objeto e mostra na tela.
+    // MÈtodo desconstrutor que pega o tempo atual no momento da "destruiÁ„o" do objeto,
+    // calcula a duraÁ„o total da vida do objeto e mostra na tela.
     ~Timer(){
         end = clock();
         duration = ((double)(end - start)) / CLOCKS_PER_SEC;
@@ -32,20 +32,20 @@ struct Timer {
     }
 };
 
-// Prot√≥tipo das fun√ß√µes do programa
+// ProtÛtipo das funÁıes do programa
 
 void printList(int list[], int size);
 
 void teste(std::ifstream &infoDesordFile);
 
-// Prot√≥tipos das fun√ß√µes de teste para cada m√©todo de ordena√ß√£o
+// ProtÛtipos das funÁıes de teste para cada mÈtodo de ordenaÁ„o
 void testeMerge(int *array);
 void testeInsertion(int *array);
 void testeQuick(int *array);
 
 const int TAM = 100000;
 
-// M√©todo principal
+// MÈtodo principal
 int main(){
 
     // Teste com lista 100% desordenada:
@@ -80,9 +80,9 @@ int main(){
 
 void teste(std::ifstream &infoDesordFile){
 
-    // Declara√ß√£o dos vetores de valores desordenados para
-    // cada m√©todo de ordena√ß√£o. Isso √© necess√°rio pois quando
-    // um array est√° ordenado ele n√£o serve mais para os testes.
+    // DeclaraÁ„o dos vetores de valores desordenados para
+    // cada mÈtodo de ordenaÁ„o. Isso · necess·rio pois quando
+    // um array est· ordenado ele n„o serve mais para os testes.
     int infoDesord_00[TAM], infoDesord_01[TAM], infoDesord_02[TAM];
 
     // Preenchendo os arrays com os valores do arquivo
@@ -107,35 +107,35 @@ void teste(std::ifstream &infoDesordFile){
 
 }
 
-// Fun√ß√£o de teste do merge sort
+// FunÁ„o de teste do merge sort
 void testeMerge(int *array){
 
     std::cout << "Merge Sort:\n";
-    // Declara√ß√£o do timer para contar o tempo da fun√ß√£o mergeSort
+    // DeclaraÁ„o do timer para contar o tempo da funÁ„o mergeSort
     Timer timer;
     mergeSort(array, 0, TAM);
 }
 
-// Fun√ß√£o de teste do insertion sort
+// FunÁ„o de teste do insertion sort
 void testeInsertion(int *array){
 
     std::cout << "Insertion Sort:\n";
-    // Declara√ß√£o do timer para contar o tempo da fun√ß√£o insertionSort
+    // DeclaraÁ„o do timer para contar o tempo da funÁ„o insertionSort
     Timer timer;
     insertionSort(array, TAM);
 }
 
-// Fun√ß√£o de teste do quick sort
+// FunÁ„o de teste do quick sort
 void testeQuick(int *array){
 
     std::cout << "Quick Sort:\n";
-    // Declara√ß√£o do timer para contar o tempo da fun√ß√£o testeSort
+    // DeclaraÁ„o do timer para contar o tempo da funÁ„o testeSort
     Timer timer;
     quickSort(array, TAM);
 }
 
-// Fun√ß√£o que lista todos os itens de um array
-// isso √© usado para debug
+// FunÁ„o que lista todos os itens de um array
+// isso È usado para debug
 void printList(int list[], int size){
     for (int i = 0; i < size; i++) {
         std::cout << "list[" << i << "] = " << list[i] << std::endl;
